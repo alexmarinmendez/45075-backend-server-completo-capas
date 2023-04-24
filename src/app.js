ba13764b109861b8d9ba781a8dfc5aa5583deab4
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-24 18:41:48 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import userRouter from './routes/users.js'
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(express.json())
+app.use('/users', userRouter)
+
+app.listen(PORT, () => console.log(`Server Up on port ${PORT}`))
